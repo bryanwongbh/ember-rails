43661bd588283ac0ce356dbeda297bf75debcd28
Bryan <bwbh87@gmail.com>
2015-12-11 16:40:43 +0800

    added template for new contact and successfully deleted contact

--- front-end/app/routes/contacts/index.js
+++ front-end/app/routes/contacts/index.js
@@ -6,5 +6,14 @@ export default Ember.Route.extend({
   // }
   model(params) {
     return this.store.query('contact', params);
-  }
+  },
+	actions: {
+	  deleteContact: function(user) {
+	  	user.destroyRecord().then(() => {
+	  		this.transitionTo('index');
+	 	 	}).catch((reason) => {
+	 	 		contact.reload();
+	 	 	});
+	 	}
+	}
 });
