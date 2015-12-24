ef596649611f5d521295389690a8efd94d03c32f
Bryan <bwbh87@gmail.com>
2015-12-24 17:13:35 +0800

    added ember simple auth devise and it worked

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'users'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount API::Base, at: "/"
