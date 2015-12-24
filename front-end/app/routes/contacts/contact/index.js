98c8e23eb43eda76c343ceb4d906e5bb381c4b77
Bryan <bwbh87@gmail.com>
2015-12-24 14:33:00 +0800

    removed model hook for single contact as it inherit from parent

--- front-end/app/routes/contacts/contact/index.js
+++ front-end/app/routes/contacts/contact/index.js
@@ -1,15 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // setupController: function( controller, model ) {
-  //   controller.set( "model", this.store.fetch( 'contact', model.id ) );
-  // }
+
+  // don't need to specify model hook because this fella inherit from parent!!
+  
   // model: function(params) {
   //   return this.store.find('contact', params.contact_id);
   // }
-  model(params) {
-  	console.log("Hello from route!!" + this._super(...arguments).get('id'));
-    return this.store.find('contact', this._super(...arguments).get('id'));
-
-  }
+  // model(params) {
+  // 	console.log("Hello from route!!" + this._super(...arguments).get('id'));
+  //   return this.store.find('contact', this._super(...arguments).get('id'));
+  // }
 });
